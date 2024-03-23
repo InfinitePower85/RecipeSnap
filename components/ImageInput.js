de37bbfca9cf9e33098858422df80acbbfd3a468
Terry Nguyen <100549717+terrykn@users.noreply.github.com>
2024-03-23 16:12:06 -0400

    fix image button

--- components/ImageInput.js
+++ components/ImageInput.js
@@ -46,10 +46,6 @@ export default function ImageInput() {
   
     return ( 
         <View style={styles.container}> 
-            <Text style={styles.header}> 
-                Snap Image
-            </Text> 
-  
             {/* Button to choose an image */} 
             <TouchableOpacity style={styles.button} 
                 onPress={pickImage}> 
@@ -81,41 +77,21 @@ const styles = StyleSheet.create({
         padding: 2, 
     }, 
     header: { 
-        fontSize: 20, 
-        marginBottom: 16, 
+
     }, 
     button: { 
-        backgroundColor: "#3cb371", 
-        padding: 10, 
-        borderRadius: 8, 
-        marginBottom: 16, 
-        shadowColor: "#000000", 
-        shadowOffset: { width: 0, height: 2 }, 
-        shadowOpacity: 0.4, 
-        shadowRadius: 4, 
-        elevation: 5, 
+
     }, 
     buttonText: { 
-        color: "#FFFFFF", 
-        fontSize: 16, 
-        fontWeight: "bold", 
+
     }, 
     imageContainer: { 
-        borderRadius: 8, 
-        marginBottom: 4, 
-        shadowColor: "#000000", 
-        shadowOffset: { width: 0, height: 2 }, 
-        shadowOpacity: 0.4, 
-        shadowRadius: 4, 
-        elevation: 5, 
+
     }, 
     image: { 
-        width: 200, 
-        height: 200, 
-        borderRadius: 8, 
+
     }, 
     errorText: { 
-        color: "red", 
-        marginTop: 16, 
+
     }, 
 });
