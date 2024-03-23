04f6073902ff5a37508ead02085494368ef98172
Andy C <85andyc@gmail.com>
2024-03-23 14:50:28 -0400

    addedLogo
    Description

---
+++ components/logo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Image } from 'react-native'
+
+// Separate component for Logo
+const Logo = () => {
+    return (
+      <Image
+        source={require('../assets/RecipeSnap.png')}
+        style={{ justifyContent: 'center', alignItems: 'center', width: 176, height: 220, marginTop: 5, zIndex:1 }}
+      />
+    );
+  };
+
+export default Logo;
--- screens/home.js
+++ screens/home.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, ScrollView } from 'react-native';
 import { Button } from 'react-native';
+import Logo from '../components/logo';
+
 const HomeScreen = ({route}) => {
     return (
         <View style={styles.container}>
+            <Logo></Logo>
         <Text>Open up App.js to start working on your app!</Text>
         <Button
         title="Go to Jane's profile"
