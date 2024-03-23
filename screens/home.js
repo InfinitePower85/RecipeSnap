84fce187f49b968a520e46cc49b42a5a9b246ebe
Terry Nguyen <100549717+terrykn@users.noreply.github.com>
2024-03-23 18:01:03 -0400

    camera and image buttons broken

--- screens/home.js
+++ screens/home.js
@@ -5,13 +5,20 @@ import Logo from '../components/logo';
 import baseStyle from '../styles/baseStyles';
 import { TouchableOpacity } from 'react-native';
 import { navbarStyles, Navbar } from '../components/navbar';
+import ImageInput from '../components/ImageInput';
+import Camera from '../components/CameraInput';
 
 const HomeScreen = ({route}) => {
     return (
+        
+
         <SafeAreaView style={[baseStyle.safeArea]}>
 
             <ScrollView contentContainerStyle={[baseStyle.container, {backgroundColor: '#f5fbe2', flexGrow: 1}]}>
                 <Logo></Logo>
+
+                <ImageInput />
+                <Camera />
                 <Text>This is the home screen!!!!</Text>
 
                 <TouchableOpacity 
