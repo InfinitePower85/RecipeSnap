452bee601a6be7232637014809eed2377c509461
Andy C <85andyc@gmail.com>
2024-03-23 15:50:57 -0400

    navbar fix

--- screens/home.js
+++ screens/home.js
@@ -4,24 +4,30 @@ import { Button } from 'react-native';
 import Logo from '../components/logo';
 import baseStyle from '../styles/baseStyles';
 import { TouchableOpacity } from 'react-native';
+import { navbarStyles, Navbar } from '../components/navbar';
 
 const HomeScreen = ({route}) => {
     return (
-        <ScrollView contentContainerStyle={[baseStyle.container, {backgroundColor: '#f5fbe2', flexGrow: 1}]}>
-            <Logo></Logo>
-            <Text>Open up App.js to start working on your app!</Text>
+        <SafeAreaView style={baseStyle.container}>
 
-            <TouchableOpacity 
-                onPress={() => route.navigate('Profile', {name: 'Jane'})}
-                style={baseStyle.button}
-            > 
-            
-            <Text> Do something </Text> 
-            <View style={styles.navbarContainer}>
-              
-            </View>
-            </TouchableOpacity>
-        </ScrollView>
+            <ScrollView contentContainerStyle={[baseStyle.container, {backgroundColor: '#f5fbe2', flexGrow: 1}]}>
+                <Logo></Logo>
+                <Text>Open up App.js to start working on your app!</Text>
+
+                <TouchableOpacity 
+                    onPress={() => route.navigate('Profile', {name: 'Jane'})}
+                    style={baseStyle.button}
+                > 
+                
+                <Text> Do something </Text> 
+                </TouchableOpacity>
+                
+            </ScrollView>
+            <View style={navbarStyles.navbarContainer}></View>
+        </SafeAreaView>
+
+
+
     );
   };
   const styles = StyleSheet.create({
