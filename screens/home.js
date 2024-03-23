573f2527905a46c554a418251417878f28753a6f
Terry Nguyen <100549717+terrykn@users.noreply.github.com>
2024-03-23 18:07:55 -0400

    aaa

--- screens/home.js
+++ screens/home.js
@@ -1,35 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, ScrollView } from 'react-native';
 import { Button } from 'react-native';
-import {Logo, logoStyles} from '../components/logo';
+import Logo from '../components/logo';
 import baseStyle from '../styles/baseStyles';
 import { TouchableOpacity } from 'react-native';
-import { navbarStyles, Navbar } from '../components/navbar';
-import ImageInput from '../components/ImageInput';
-import Camera from '../components/CameraInput';
 
 const HomeScreen = ({route}) => {
     return (
-        <SafeAreaView style={[baseStyle.safeArea]}>
+        <ScrollView contentContainerStyle={[baseStyle.container, {backgroundColor: '#f5fbe2', flexGrow: 1}]}>
+            <Logo></Logo>
+            <Text>Open up App.js to start working on your app!</Text>
 
-            <ScrollView contentContainerStyle={[baseStyle.container, {backgroundColor: '#f5fbe2', flexGrow: 1}]}>
-                <Logo></Logo>
-                <Text>This is the home screen!!!!</Text>
-
-                <TouchableOpacity 
-                    onPress={() => route.navigate('Profile', {name: 'Jane'})}
-                    style={baseStyle.button}
-                > 
-                
-                <Text> Do something </Text> 
-                </TouchableOpacity>
-                
-            </ScrollView>
-            <Navbar></Navbar>
-        </SafeAreaView>
-
-
-
+            <TouchableOpacity 
+                onPress={() => route.navigate('Profile', {name: 'Jane'})}
+                style={baseStyle.button}
+            > 
+            
+            <Text> Do something </Text> 
+            
+            </TouchableOpacity>
+        </ScrollView>
     );
   };
   const styles = StyleSheet.create({
