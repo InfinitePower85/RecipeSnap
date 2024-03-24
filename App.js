32da2b913c94f774d6fdfe532a36275d1cce1008
Andy C <85andyc@gmail.com>
2024-03-23 22:27:12 -0400

    Differentiated home screen and camera screen

--- App.js
+++ App.js
@@ -5,17 +5,19 @@ import LoginScreen from './screens/login';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ExampleComponent from './screens/colorButton';
 import PokemonInfo from './screens/pokemonAPI';
+import CameraScreen from './screens/CameraScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator  initialRouteName="Home">
+      <Stack.Navigator  initialRouteName="CameraScreen">
         <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/> 
         <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false}} />
         <Stack.Screen name="eee" component={ExampleComponent} options={{headerShown: false}} />
         <Stack.Screen name="pokem" component={PokemonInfo} options={{headerShown: false}} />
+        <Stack.Screen name="Camera" component={CameraScreen} options={{headerShown: false}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
